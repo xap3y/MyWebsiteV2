af87e958270428fa8f477d3d3204979d3166c3a7
XAP3Y <github@xap3y.eu>
2025-01-25 20:24:52 +0100

    deployment fix

--- src/app/index2/page.tsx
+++ src/app/index2/page.tsx
@@ -1,18 +1,12 @@
 import {Navigation} from "@/components/navigation";
 import React from "react";
 
-interface PageProps {
-    params: {
-        value: string,
-        setValue: React.Dispatch<React.SetStateAction<string>>
-    };
-}
 
-export default function Page({params}: PageProps) {
+export default function Page() {
 
     return (
         <>
-            <Navigation value={params.value} setValue={params.setValue} animations={false} />
+            <Navigation animations={false} />
         </>
     )
 }
